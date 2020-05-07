4ae19707f5f38abecc65440f6ac876e21d542a61
BouchraCHLIHFANE <63127829+BouchraCHLIHFANE@users.noreply.github.com>
2020-05-08 00:19:15 +0100

    version sidebar v0

--- src/app/ajouter-personne/ajouter-personne.component.ts
+++ src/app/ajouter-personne/ajouter-personne.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { PersonneService } from '../personne.service';
-import { PatientService } from '../patient.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Personne } from '../personne';
 import { Patient } from '../patient';
-import {NgForm} from '@angular/forms';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { PatientService } from '../patient.service';
+
+
 @Component({
   selector: 'app-ajouter-pers',
   templateUrl: './ajouter-personne.component.html',
@@ -15,42 +14,45 @@ export class AjouterPersonneComponent implements OnInit {
   patient : Patient = new Patient ();
   submitted =false;
   formGroup: FormGroup;
+  
 
+  
+    
 
-  constructor(private patientService :PatientService,
-    private router: Router,private formBuilder: FormBuilder) {}
-
-   
-
-   onSubmit() {
+constructor(private patientService :PatientService,
+private router: Router,private formBuilder: FormBuilder) {} 
+ngOnInit(): void {}
+  
+  onSubmit() {
       this.submitted = true;
-      this.patient = new Patient(this.formGroup.value);
+    // this.patient = new Patient();
       this.save();
     }
-  ngOnInit(): void {
+ /* ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       'email' : new FormControl(''),
       'nom' : new FormControl(''),
       'role' : new FormControl(''),
       'civilite' : new FormControl('')
         });
-  }
-  save (){
+  } */
 
+  save (){
+    
     //add patient
     this.patientService.savePatient(this.patient).subscribe(
       data => console.log(data),
-      error => console.log(error)
+      error => console.log(error))
+      this. gotoList(); 
+    }
    
-       );
-    this. gotoList();
-  }
   
+  /*
   newPatient(): void {
     this.submitted = false;
     this.patient = new Patient();
   }
-
+ */
   gotoList() {
     this.router.navigate(['/personne']);
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TooltipConfig } from 'ngx-bootstrap/tooltip';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: TooltipConfig, useFactory: getAlertConfig }]
 })
+
 export class AppComponent {
   title = 'ProjAng-front';
+} 
+//ljadid
+
+
+export function getAlertConfig(): TooltipConfig {
+  return Object.assign(new TooltipConfig(), {
+    placement: 'right',
+    container: 'body'
+  });
 }
